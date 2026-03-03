a25cec36f33841ead413fc940fe90085f16687ac
taras <nomail@nomail>
2026-03-03 22:11:08 +0100

    Update

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import sys
 from pathlib import Path
 
@@ -17,9 +18,30 @@ else:
 
 openssl_libraries = ["ssl", "crypto"]
 
+def _find_macos_openssl_prefixes():
+    prefixes = []
+    for formula in ("openssl", "openssl@3"):
+        try:
+            out = subprocess.check_output(
+                ["brew", "--prefix", formula],
+                text=True,
+                stderr=subprocess.DEVNULL,
+            ).strip()
+        except (subprocess.CalledProcessError, FileNotFoundError):
+            continue
+        if out and out not in prefixes:
+            prefixes.append(out)
+    return prefixes
+
+
 if sys.platform == "darwin":
-    openssl_link_dirs = [str(Path(sys.prefix) / 'lib')]
+    openssl_prefixes = _find_macos_openssl_prefixes()
+    assert openssl_prefixes, "could not find OpenSSL"
+    openssl_include_dirs = [str(Path(p) / "include") for p in openssl_prefixes]
+    openssl_link_dirs = [str(Path(sys.prefix) / "lib")]
+    # openssl_link_dirs = [str(Path(p) / "lib") for p in openssl_prefixes]
 else:
+    openssl_include_dirs = []
     openssl_link_dirs = []
 
 extensions = [
@@ -29,6 +51,7 @@ extensions = [
               libraries=base_libraries),
     Extension("aiofastnet.sslproto", ["aiofastnet/sslproto.pyx", "aiofastnet/static_mem_bio.c", "aiofastnet/certdecode.c"],
               libraries=base_libraries + openssl_libraries,
+              include_dirs=openssl_include_dirs,
               library_dirs=openssl_link_dirs),
     Extension("aiofastnet.sslproto_stdlib", ["aiofastnet/sslproto_stdlib.pyx"],
               libraries=base_libraries),
