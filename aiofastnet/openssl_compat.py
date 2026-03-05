6e516974226f904cf43e22e12e896dabc15a2e69
taras <nomail@nomail>
2026-03-05 10:28:28 +0100

    Update params

--- aiofastnet/openssl_compat.py
+++ aiofastnet/openssl_compat.py
@@ -17,40 +17,58 @@ def _pick_library(base_dir: Path, prefix: str, suffix: str):
 
 
 def _find_openssl_library_paths():
-    roots = []
-    for root in (
-        Path(sys.prefix) / "libs",
-        Path(getattr(sys, "base_prefix", sys.prefix)) / "libs",
-        Path(sys.prefix) / "lib",
-        Path(getattr(sys, "base_prefix", sys.prefix)) / "lib",
-    ):
-        if root not in roots:
-            roots.append(root)
-
-    if os.name == "nt":
-        ssl_suffix = ".dll"
-        crypto_suffix = ".dll"
-    elif sys.platform == "darwin":
-        ssl_suffix = ".dylib"
-        crypto_suffix = ".dylib"
+    if sys.platform == "darwin":
+        if sys.version_info < (3, 11):
+            return "libssl.dylib.1", "libcrypto.dylib.1"
+        else:
+            return "libssl.dylib.3", "libcrypto.dylib.3"
+    elif sys.platform in ("linux", "aix", "freebsd"):
+        if sys.version_info < (3, 11):
+            return "libssl.so.1", "libcrypto.so.1"
+        else:
+            return "libssl.so.3", "libcrypto.so.3"
+    elif sys.platform == "win32":
+        if sys.version_info < (3, 11):
+            return "libssl-3.dll", "libcrypto-3.dll"
+        else:
+            return "libssl-1.dll", "libcrypto-1.dll"
     else:
-        ssl_suffix = ".so"
-        crypto_suffix = ".so"
+        return ImportError(f"unsupported platform: {sys.platform}")
 
-    ssl_path = None
-    crypto_path = None
-    for root in roots:
-        if ssl_path is None:
-            ssl_path = _pick_library(root, "libssl", ssl_suffix)
-        if crypto_path is None:
-            crypto_path = _pick_library(root, "libcrypto", crypto_suffix)
-        if ssl_path is not None and crypto_path is not None:
-            break
-
-    if ssl_path is None or crypto_path is None:
-        raise ImportError(
-            "aiofastnet: could not find OpenSSL libraries "
-            f"(ssl={ssl_path!r}, crypto={crypto_path!r}) in roots={roots!r}"
-        )
-
-    return ssl_path, crypto_path
+    # roots = []
+    # for root in (
+    #     Path(sys.prefix) / "libs",
+    #     Path(getattr(sys, "base_prefix", sys.prefix)) / "libs",
+    #     Path(sys.prefix) / "lib",
+    #     Path(getattr(sys, "base_prefix", sys.prefix)) / "lib",
+    # ):
+    #     if root not in roots:
+    #         roots.append(root)
+    #
+    # if os.name == "nt":
+    #     ssl_suffix = ".dll"
+    #     crypto_suffix = ".dll"
+    # elif sys.platform == "darwin":
+    #     ssl_suffix = ".dylib"
+    #     crypto_suffix = ".dylib"
+    # else:
+    #     ssl_suffix = ".so"
+    #     crypto_suffix = ".so"
+    #
+    # ssl_path = None
+    # crypto_path = None
+    # for root in roots:
+    #     if ssl_path is None:
+    #         ssl_path = _pick_library(root, "libssl", ssl_suffix)
+    #     if crypto_path is None:
+    #         crypto_path = _pick_library(root, "libcrypto", crypto_suffix)
+    #     if ssl_path is not None and crypto_path is not None:
+    #         break
+    #
+    # if ssl_path is None or crypto_path is None:
+    #     raise ImportError(
+    #         "aiofastnet: could not find OpenSSL libraries "
+    #         f"(ssl={ssl_path!r}, crypto={crypto_path!r}) in roots={roots!r}"
+    #     )
+    #
+    # return ssl_path, crypto_path
