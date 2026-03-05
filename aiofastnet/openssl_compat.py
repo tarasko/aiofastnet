e7f105629f8ba45b0a46fa91c29fd9bafb46e42a
taras <nomail@nomail>
2026-03-05 13:33:48 +0100

    Build

--- aiofastnet/openssl_compat.py
+++ aiofastnet/openssl_compat.py
@@ -1,11 +1,148 @@
+import ctypes
 import ctypes.util
 import sys
 import os
 
 
 if sys.version_info < (3, 14):
-    dllist = None
-    pass
+    if os.name == "nt":
+        # Listing loaded DLLs on Windows relies on the following APIs:
+        # https://learn.microsoft.com/windows/win32/api/psapi/nf-psapi-enumprocessmodules
+        # https://learn.microsoft.com/windows/win32/api/libloaderapi/nf-libloaderapi-getmodulefilenamew
+        from ctypes import wintypes
+
+        _kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
+        _get_current_process = _kernel32["GetCurrentProcess"]
+        _get_current_process.restype = wintypes.HANDLE
+
+        _k32_get_module_file_name = _kernel32["GetModuleFileNameW"]
+        _k32_get_module_file_name.restype = wintypes.DWORD
+        _k32_get_module_file_name.argtypes = (
+            wintypes.HMODULE,
+            wintypes.LPWSTR,
+            wintypes.DWORD,
+        )
+
+        # gh-145307: We defer loading psapi.dll until _get_module_handles is called.
+        # Loading additional DLLs at startup for functionality that may never be
+        # used is wasteful.
+        _enum_process_modules = None
+
+        def _get_module_filename(module: wintypes.HMODULE):
+            name = (wintypes.WCHAR * 32767)()  # UNICODE_STRING_MAX_CHARS
+            if _k32_get_module_file_name(module, name, len(name)):
+                return name.value
+            return None
+
+
+        def _get_module_handles():
+            global _enum_process_modules
+            if _enum_process_modules is None:
+                _psapi = ctypes.WinDLL('psapi', use_last_error=True)
+                _enum_process_modules = _psapi["EnumProcessModules"]
+                _enum_process_modules.restype = wintypes.BOOL
+                _enum_process_modules.argtypes = (
+                    wintypes.HANDLE,
+                    ctypes.POINTER(wintypes.HMODULE),
+                    wintypes.DWORD,
+                    wintypes.LPDWORD,
+                )
+
+            process = _get_current_process()
+            space_needed = wintypes.DWORD()
+            n = 1024
+            while True:
+                modules = (wintypes.HMODULE * n)()
+                if not _enum_process_modules(process,
+                                             modules,
+                                             ctypes.sizeof(modules),
+                                             ctypes.byref(space_needed)):
+                    err = ctypes.get_last_error()
+                    msg = ctypes.FormatError(err).strip()
+                    raise ctypes.WinError(err,
+                                          f"EnumProcessModules failed: {msg}")
+                n = space_needed.value // ctypes.sizeof(wintypes.HMODULE)
+                if n <= len(modules):
+                    return modules[:n]
+
+        def dllist():
+            """Return a list of loaded shared libraries in the current process."""
+            modules = _get_module_handles()
+            libraries = [name for h in modules
+                         if (name := _get_module_filename(h)) is not None]
+            return libraries
+
+    elif os.name == "posix" and sys.platform in {"darwin", "ios", "tvos", "watchos"}:
+        from ctypes.macholib.dyld import dyld_find as _dyld_find
+
+        def find_library(name):
+            possible = ['lib%s.dylib' % name,
+                        '%s.dylib' % name,
+                        '%s.framework/%s' % (name, name)]
+            for name in possible:
+                try:
+                    return _dyld_find(name)
+                except ValueError:
+                    continue
+            return None
+
+
+        # Listing loaded libraries on Apple systems relies on the following API:
+        # https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man3/dyld.3.html
+        import ctypes
+
+        _libc = ctypes.CDLL(find_library("c"))
+        _dyld_get_image_name = _libc["_dyld_get_image_name"]
+        _dyld_get_image_name.restype = ctypes.c_char_p
+
+
+        def dllist():
+            """Return a list of loaded shared libraries in the current process."""
+            num_images = _libc._dyld_image_count()
+            libraries = [os.fsdecode(name) for i in range(num_images)
+                         if (name := _dyld_get_image_name(i)) is not None]
+
+            return libraries
+    if (os.name == "posix" and sys.platform not in {"darwin", "ios", "tvos", "watchos"}):
+        if hasattr((_libc := ctypes.CDLL(None)), "dl_iterate_phdr"):
+            class _dl_phdr_info(ctypes.Structure):
+                _fields_ = [
+                    ("dlpi_addr", ctypes.c_void_p),
+                    ("dlpi_name", ctypes.c_char_p),
+                    ("dlpi_phdr", ctypes.c_void_p),
+                    ("dlpi_phnum", ctypes.c_ushort),
+                ]
+
+            _dl_phdr_callback = ctypes.CFUNCTYPE(
+                ctypes.c_int,
+                ctypes.POINTER(_dl_phdr_info),
+                ctypes.c_size_t,
+                ctypes.POINTER(ctypes.py_object),
+            )
+
+            @_dl_phdr_callback
+            def _info_callback(info, _size, data):
+                libraries = data.contents.value
+                name = os.fsdecode(info.contents.dlpi_name)
+                libraries.append(name)
+                return 0
+
+            _dl_iterate_phdr = _libc["dl_iterate_phdr"]
+            _dl_iterate_phdr.argtypes = [
+                _dl_phdr_callback,
+                ctypes.POINTER(ctypes.py_object),
+            ]
+            _dl_iterate_phdr.restype = ctypes.c_int
+
+            def dllist():
+                """Return a list of loaded shared libraries in the current process."""
+                libraries = []
+                _dl_iterate_phdr(_info_callback,
+                                 ctypes.byref(
+                                     ctypes.py_object(libraries)))
+                return libraries
+    else:
+        dllist = None
 else:
     dllist = ctypes.util.dllist
 
