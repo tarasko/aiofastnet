eb13f67aaa21f4c7744cfb2e2ea5fb5c6a9134f0
taras <nomail@nomail>
2026-06-26 01:38:16 +0200

    Fix sendfile not being available on windows with proactor loops

--- aiofastnet/api_sendfile.py
+++ aiofastnet/api_sendfile.py
@@ -1,5 +1,4 @@
 import asyncio
-import os
 
 from .transport import Transport
 from .wrapped_transport import _WrappedTransport, _get_original_loop_method
@@ -17,15 +16,19 @@ async def sendfile(loop: asyncio.AbstractEventLoop,
     Ignores fallback argument. Always raises NotImplementedError if native
     sendfile is not available.
     """
-    if os.name == "nt":
-        raise NotImplementedError()
+    if isinstance(transport, _WrappedTransport):
+        loop_sendfile = _get_original_loop_method(loop, "sendfile")
+        return await loop_sendfile(
+            transport._transport, file, offset, count, fallback=fallback)
 
-    if isinstance(transport, Transport) and not isinstance(transport, _WrappedTransport):
+    elif isinstance(transport, Transport):
         fut = transport.sendfile(file, offset, count)
         if fut is not None:
             return await fut
         else:
             return None
+
     else:
         loop_sendfile = _get_original_loop_method(loop, "sendfile")
-        return await loop_sendfile(transport, file, offset, count)
+        return await loop_sendfile(
+            transport, file, offset, count, fallback=fallback)
