f8c44b2a43ed65950487f698596dbf4589691b09
taras <nomail@nomail>
2026-06-30 01:35:18 +0200

    Update fallback

--- aiofastnet/ssl_engine_fallback.pyx
+++ aiofastnet/ssl_engine_fallback.pyx
@@ -1,5 +1,7 @@
 import ssl
 
+from cpython.memoryview cimport PyMemoryView_FromMemory
+from cpython.buffer cimport PyBUF_READ
 from cpython.bytearray cimport PyByteArray_AS_STRING
 from cpython.bytes cimport PyBytes_AS_STRING, PyBytes_GET_SIZE, PyBytes_FromStringAndSize
 from libc.limits cimport INT_MAX
@@ -15,24 +17,29 @@ cdef object _logger = logging.getLogger('aiofastnet.ssl')
 
 cdef class SSLEngineFallback(SSLEngine):
     cdef:
+        object _incoming
+        bytearray _incoming_buf
+
+        object _outgoing
+        bytes _outgoing_data
+        Py_ssize_t _write_max_size
+
         object ssl_object
-        object incoming
-        object outgoing
-        bytearray incoming_buf
-        bytes outgoing_data
 
     def __init__(self, ssl_context, bint server_side, str server_hostname,
-                 Py_ssize_t read_buffer_size, Py_ssize_t write_buffer_size,
+                 Py_ssize_t read_buffer_size, Py_ssize_t write_max_size,
                  sock=None):
         SSLEngine.__init__(self, ssl_context, server_side, server_hostname)
 
-        self.incoming = ssl.MemoryBIO()
-        self.outgoing = ssl.MemoryBIO()
-        self.incoming_buf = bytearray(read_buffer_size)
-        self.outgoing_data = b""
+        self._incoming = ssl.MemoryBIO()
+        self._outgoing = ssl.MemoryBIO()
+        self._incoming_buf = bytearray(read_buffer_size)
+        self._outgoing_data = b""
+        self._write_max_size = 64*1024
+
         self.ssl_object = ssl_context.wrap_bio(
-            self.incoming,
-            self.outgoing,
+            self._incoming,
+            self._outgoing,
             server_side=server_side,
             server_hostname=server_hostname,
         )
@@ -104,7 +111,7 @@ cdef class SSLEngineFallback(SSLEngine):
 
         bytes_read[0] = 0
         while buf_len != 0:
-            if self.ssl_object.pending() == 0 and self.incoming.pending == 0:
+            if self.ssl_object.pending() == 0 and self._incoming.pending == 0:
                 return SSLError.SSL_ERROR_WANT_READ
 
             read_len = INT_MAX if buf_len > INT_MAX else <int>buf_len
@@ -137,33 +144,44 @@ cdef class SSLEngineFallback(SSLEngine):
         return SSLError.SSL_ERROR_NONE
 
     cdef SSLError write(self, conn, char *data_ptr, Py_ssize_t data_len, Py_ssize_t* bytes_written) except SSLError.PYTHON_EXC:
+        bytes_written[0] = 0
+        if unlikely(data_len == 0):
+            return SSLError.SSL_ERROR_NONE
+
+        cdef Py_ssize_t available_for_writing = max(self._write_max_size - <Py_ssize_t>self._outgoing.pending, 0)
+        if unlikely(available_for_writing == 0):
+            return SSLError.SSL_ERROR_WANT_WRITE
+
+        # Always let to write the whole TLS record to prevent records of non-optimal size.
+        available_for_writing = max(16*1024, available_for_writing)
+
+        # We need to limit writing size, because ssl.SSLObject.write just writes until all data is written,
+        # and memory bio grows without limits
+
+        cdef Py_ssize_t bytes_to_write = min(data_len, available_for_writing)
+        data = PyMemoryView_FromMemory(data_ptr, bytes_to_write, PyBUF_READ)
+
         cdef:
-            bytes data
             Py_ssize_t last_bytes_written
             SSLError ssl_error
 
-        bytes_written[0] = 0
-        while data_len != 0:
-            data = PyBytes_FromStringAndSize(data_ptr, data_len)
-            try:
-                last_bytes_written = self.ssl_object.write(data)
-            except ssl.SSLError as exc:
-                ssl_error = self._translate_ssl_error(exc)
-                if unlikely(self._is_debug):
-                    _logger.debug("%r: SSLObject.write(data_len=%d), %s", conn, data_len, ssl_error_name(ssl_error))
-                if ssl_error in (SSLError.SSL_ERROR_WANT_READ, SSLError.SSL_ERROR_WANT_WRITE):
-                    return ssl_error
-                raise
-
-            if last_bytes_written <= 0:
-                raise RuntimeError("SSLObject.write made no progress")
-
-            if unlikely(self._is_debug):
-                _logger.debug("%r: SSLObject.write(data_len=%d)=%d", conn, data_len, last_bytes_written)
-
+        try:
+            last_bytes_written = self.ssl_object.write(data)
             bytes_written[0] += last_bytes_written
-            data_ptr += last_bytes_written
-            data_len -= last_bytes_written
+            if unlikely(self._is_debug):
+                _logger.debug("%r: SSLObject.write(data_len=%d)=%d", conn, bytes_to_write, last_bytes_written)
+        except ssl.SSLError as exc:
+            ssl_error = self._translate_ssl_error(exc)
+            if unlikely(self._is_debug):
+                _logger.debug("%r: SSLObject.write(data_len=%d), %s", conn, data_len, ssl_error_name(ssl_error))
+
+            if ssl_error in (SSLError.SSL_ERROR_WANT_READ, SSLError.SSL_ERROR_WANT_WRITE):
+                return ssl_error
+
+            raise
+
+        if last_bytes_written < data_len:
+            return SSLError.SSL_ERROR_WANT_WRITE
 
         return SSLError.SSL_ERROR_NONE
 
@@ -172,13 +190,13 @@ cdef class SSLEngineFallback(SSLEngine):
         raise NotImplementedError()
 
     cdef incoming_bio_get_write_buf(self, char **pp, Py_ssize_t *space):
-        pp[0] = PyByteArray_AS_STRING(self.incoming_buf)
-        space[0] = len(self.incoming_buf)
+        pp[0] = PyByteArray_AS_STRING(self._incoming_buf)
+        space[0] = len(self._incoming_buf)
 
     cdef incoming_bio_produce(self, Py_ssize_t nbytes):
         if nbytes == 0:
             return
-        self.incoming.write(PyBytes_FromStringAndSize(PyByteArray_AS_STRING(self.incoming_buf), nbytes))
+        self._incoming.write(PyBytes_FromStringAndSize(PyByteArray_AS_STRING(self._incoming_buf), nbytes))
 
     cdef int sendfile_available(self) except -1:
         return 0
@@ -190,26 +208,26 @@ cdef class SSLEngineFallback(SSLEngine):
         raise NotImplementedError("stdlib ssl.SSLObject does not expose renegotiation")
 
     cdef int outgoing_bio_reset(self) except -1:
-        self.outgoing_data = b""
-        while self.outgoing.pending:
-            self.outgoing.read()
+        self._outgoing_data = b""
+        while self._outgoing.pending:
+            self._outgoing.read()
         return 1
 
     cdef Py_ssize_t outgoing_bio_pending(self) except -1:
-        return PyBytes_GET_SIZE(self.outgoing_data) + self.outgoing.pending
+        return PyBytes_GET_SIZE(self._outgoing_data) + self._outgoing.pending
 
     cdef Py_ssize_t outgoing_bio_get_data(self, char** pp) except -1:
-        if PyBytes_GET_SIZE(self.outgoing_data) == 0 and self.outgoing.pending:
-            self.outgoing_data = self.outgoing.read()
+        if PyBytes_GET_SIZE(self._outgoing_data) == 0 and self._outgoing.pending:
+            self._outgoing_data = self._outgoing.read()
 
-        pp[0] = PyBytes_AS_STRING(self.outgoing_data)
-        return PyBytes_GET_SIZE(self.outgoing_data)
+        pp[0] = PyBytes_AS_STRING(self._outgoing_data)
+        return PyBytes_GET_SIZE(self._outgoing_data)
 
     cdef outgoing_bio_consume(self, Py_ssize_t nbytes):
-        cdef Py_ssize_t pending = PyBytes_GET_SIZE(self.outgoing_data)
+        cdef Py_ssize_t pending = PyBytes_GET_SIZE(self._outgoing_data)
         if nbytes > pending:
             raise RuntimeError("outgoing BIO consume size exceeds pending data")
         if nbytes == pending:
-            self.outgoing_data = b""
+            self._outgoing_data = b""
         else:
-            self.outgoing_data = self.outgoing_data[nbytes:]
+            self._outgoing_data = self._outgoing_data[nbytes:]
