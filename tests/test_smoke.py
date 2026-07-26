fb157e8bc531f5d87d832a1be1d74c1143b06fee
taras <nomail@nomail>
2026-07-26 18:55:28 +0200

    Fix ruff

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -34,7 +34,7 @@ async def test_echo_socketpair(conn_type_plus_udp):
 
     async with SocketPair(ct=conn_type_plus_udp,
                           server_protocol_factory=EchoServerProtocol,
-                          client_server_hostname="127.0.0.1") as (server, client):
+                          client_server_hostname="127.0.0.1") as (_server, client):
         client.write(payload)
         echoed = await client.readn(msg_size)
         assert echoed == payload
